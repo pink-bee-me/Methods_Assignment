d3bae17044f75c92214401b1cc4c9db457127c07
LaurieSue <pink.bee.me@outlook.com>
2020-10-10 09:06:25 -0500

    Cannot figure out Method syntax or format or placement or timing!!!

--- Methods_Assignment/User.cs
+++ Methods_Assignment/User.cs
@@ -6,51 +6,41 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Methods_Assignment
-{
+
     public class User
     {
-        public User user(string userName, int userAge)
+        public User User(string userName, int userAge)
         {
             this.UserName = userName;
             this.UserAge = userAge;
-            
-            DriversLicense;
         }
 
-        public string UserName { get; set; }
-        public int UserAge { get; set; }
-    
+        
+        public string UserName {get; set;}
+        public int UserAge {get; set;}
 
+        {
         //MATERIAL FOR THE OTHER METHODS
         //public bool VoteAge = userAge => 18;
         //public bool AlcoholTobaccoAge = userAge => 21;
         //public bool PresidentOfUnitedStatesAge = userAge => 35;
         //public bool SeniorDiscountAge = 65;
+    
 
-        public User DriversLicense(string userName, int userAge)
+ {   
+       public User DriversLicense(string userName, int userAge)
 
         {
-            public bool passValidation = true;
-       int driversLicenseAge = 16;
-            int result = yearsToGo;
 
-            {
-                if (UserAge => driversLicenseAge)
-                {
-                    if (isPassValidation = true)
-                    {
-                        PassValidation = 1; {
-                            Console.WriteLine("Validation for: " + UserName + "\n Age: " + UserAge + "\n Confirmed ");
-                        }
-                    } ]
-                    else
-                    {
-                        int yearsToGo = userAge - driversLicenseAge;
-                        Console.WriteLine("Validation for: " + UserName +
-                             "\n Age: " + UserAge +
-                             "\n\n Not Confirmed !!!" +
-                             " \n Age Eligibility Requirements Will Be Met In : " + yearsToGo + " years".);
-                    }
+            if (userAge < 16)
+        {
+            int yearsToGo = userAge - 16;
+            Console.WriteLine("Validation for: " + userName +
+                 "\n Age: " + userAge +
+                 "\n\n Not Confirmed !!!" +
+                 " \n Age Eligibility Requirements Will Be Met In : " + yearsToGo + " years.");
+        } 
+    } 
+}
 
             
