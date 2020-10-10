5149ccd1d7b15b210c3f1caec180392e888b9809
LaurieSue <pink.bee.me@outlook.com>
2020-10-10 08:26:47 -0500

    Add project files.

---
+++ Methods_Assignment/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Enter Your Name:");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Enter Your Age:");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+            User user = new User(userName, userAge)
+        }
+    }//run the drivers licence method
+    //run voting method
+    // run drinking and tabacco method
+    //run president method
+    //run seenior citizen method
+
+    // all methods to return if the user is old enough (age validation to receive benefit ) AND
+    //also return the integer value of how many years until they do reach the right age 
+}   // that's the plan anyways...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Methods_Assignment/User.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Assignment
+{
+    public class User
+    {
+        public User user(string userName, int userAge)
+        {
+            this.UserName = userName;
+            this.UserAge = userAge;
+            
+            DriversLicense;
+        }
+
+        public string UserName { get; set; }
+        public int UserAge { get; set; }
+    
+
+        //MATERIAL FOR THE OTHER METHODS
+        //public bool VoteAge = userAge => 18;
+        //public bool AlcoholTobaccoAge = userAge => 21;
+        //public bool PresidentOfUnitedStatesAge = userAge => 35;
+        //public bool SeniorDiscountAge = 65;
+
+        public User DriversLicense(string userName, int userAge)
+
+        {
+            public bool passValidation = true;
+       int driversLicenseAge = 16;
+            int result = yearsToGo;
+
+            {
+                if (UserAge => driversLicenseAge)
+                {
+                    if (isPassValidation = true)
+                    {
+                        PassValidation = 1; {
+                            Console.WriteLine("Validation for: " + UserName + "\n Age: " + UserAge + "\n Confirmed ");
+                        }
+                    } ]
+                    else
+                    {
+                        int yearsToGo = userAge - driversLicenseAge;
+                        Console.WriteLine("Validation for: " + UserName +
+                             "\n Age: " + UserAge +
+                             "\n\n Not Confirmed !!!" +
+                             " \n Age Eligibility Requirements Will Be Met In : " + yearsToGo + " years".);
+                    }
+
+            
